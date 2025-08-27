dad46c88325b10a2a6b40711b72fb66c938a388b
titlmichal <titlmichal@gmail.com>
2025-08-27 19:43:44 +0200

    Start of scraper build: get html contents of specified website, parse the tags, identify car items

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup, Tag
+
+r = requests.get("https://www.sauto.cz/inzerce/osobni")
+if r.status_code != 200:
+    raise Exception("Error. Request unsuccesful!")
+
+html_soup = BeautifulSoup(r.content, "html.parser")
+# html_soup.prettify
+# print(html_soup.body.contents)
+html_body = html_soup.find("body")
+counter = 0
+for descendant in html_body.descendants:   
+    # descendants are all indirect children but that all means
+    # ...that even simple strings within the tags are indirect children
+    if type(descendant) != Tag:
+        continue
+    # print(descendant)
+
+    if "class" in descendant.attrs.keys():
+        # tags attributes are stored like dicts
+        if len(descendant.attrs["class"]) > 0:
+            # if attribute value of class = "" it will be counted in len but not found via index
+            if "c-item__data-wrap" in descendant.attrs["class"]:
+                # this class should be unique identifier of car item --> DOUBLE CHECK THAT
+                print(descendant)
+                print()
+    else:
+        continue
+    # print(descendant.attrs.keys())
+    # print(counter)
+    
+    counter += 1
+    # print()
